bc33ceec77d1132c394bd63323536b34e544991a
hoodielive <hoodielive@gmail.com>
2018-03-19 20:08:01 -0400

    Edit on deck.go main.go

--- cards/deck.go
+++ cards/deck.go
@@ -4,8 +4,27 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	// this function does not require a receiver
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	// don't forget to return cards 
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
