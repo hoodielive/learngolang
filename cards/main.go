15f47faa4f99da5f3059cc3b2578eab58a579a7b
hoodielive <hoodielive@gmail.com>
2018-03-31 13:52:43 -0400

    working with conversion

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,12 @@
 package main
 
+import "fmt" 
 func main() {
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-	hand.print() 
-	remainingCards.print()
+//	cards := newDeck()
+//
+//	hand, remainingCards := deal(cards, 5)
+//	hand.print() 
+//	remainingCards.print()
+	greeting := "hi there!"
+	fmt.Println([]byte(greeting)) 
 }
