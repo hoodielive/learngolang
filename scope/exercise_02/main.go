de7414152550850c167152cfd9ef77f5906c9191
hoodielive <hoodielive@gmail.com>
2018-09-30 19:06:05 -0400

    scope exercise 1 and 2

---
+++ scope/exercise_02/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string = "Hi, Hey, Sup?, What's up, What it do?"
+	fmt.Println(x)
+	f();
+}
+
+func f() {
+	x := "some string"
+	fmt.Println(x)
+}
+
