3cb61d13161ef559fa23a37abcd6be0b54c5806b
hoodielive <hoodielive@gmail.com>
2018-09-30 18:25:36 -0400

    Go concepts and recipes

---
+++ strings/main.go
@@ -0,0 +1,9 @@
+package main 
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello, Someone!"))
+	fmt.Println("Hello, Everyone!"[1])
+	fmt.Println("Hello, " + "Aeon!")
+}
---
+++ variables/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string 
+
+	x = "first"
+	
+	fmt.Println(x)
+
+	x = "second"
+	
+	fmt.Println(x)
+
+	x = x + " " + "second"
+
+	fmt.Println(x)
+
+	fmt.Println(x == x)
+}
+
