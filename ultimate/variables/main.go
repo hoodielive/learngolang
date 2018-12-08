acb80b843dd1dbeaf1295652b1fa4813b1213625
hoodielive <hoodielive@gmail.com>
2018-12-08 08:32:26 -0500

    zero value 0

--- ultimate/variables/main.go
+++ ultimate/variables/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 /* declare, assign, initiliaze */
 
-var b string // this is declaration; b = "golang" is assignment
+var b string // this is declaration; b = "golang" is assignment; this would be declaring a variable to its zero value
 
 func main() {
 
