86adcfbaf1ba7c05ea9a5a51f01e81f82d54dbd8
hoodielive <hoodielive@gmail.com>
2019-03-04 20:26:36 -0500

    this is probably all I'm doing today lol

---
+++ inancgumus/src/second/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  var speed int
+  var heat float64
+  var off bool
+  var brand string
+
+  fmt.Println(speed)
+  fmt.Println(heat)
+  fmt.Println(off)
+  fmt.Printf("%q\n", brand)
+}
